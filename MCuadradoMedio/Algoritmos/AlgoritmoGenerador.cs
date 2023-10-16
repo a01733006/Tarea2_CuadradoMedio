c88173e90ce3454ebc30d8e019a65b1f6e1eaa29
pedro <pedro@LAPTOP-DMRF6SEK>
2023-10-15 23:51:30 -0600

    Add project files.

---
+++ MCuadradoMedio/Algoritmos/AlgoritmoGenerador.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCuadradoMedio.Algoritmos
+{
+    public class AlgoritmoGenerador
+    {
+        public List<Tuple<int, int, int, double, double>> MetodoCuadradosMedios(int semilla)
+        {
+            List<Tuple<int, int, int, double, double>> resultados = new List<Tuple<int, int, int, double, double>>();
+            int semillaActual = semilla;
+            int iteracion = 0;
+
+            while (semillaActual >= 0)
+            {
+                int cuadrado = semillaActual * semillaActual;
+                string cuadradoStr = cuadrado.ToString();
+                string valor1Str = "";
+                string valor2Str = "";
+
+                if (cuadradoStr.Length == 5)
+                {
+                    valor1Str = cuadradoStr.Substring(1, 3);
+                    valor2Str = "0";
+                }
+                else if (cuadradoStr.Length == 6)
+                {
+                    valor1Str = cuadradoStr.Substring(1, 3);
+                    valor2Str = cuadradoStr.Substring(2, 3);
+                }
+
+                double valor1 = 0;
+                if (!string.IsNullOrEmpty(valor1Str) && double.TryParse(valor1Str, out double tempVal1))
+                {
+                    valor1 = tempVal1;
+                }
+
+                double valor2 = 0;
+                if (!string.IsNullOrEmpty(valor2Str) && double.TryParse(valor2Str, out double tempVal2))
+                {
+                    valor2 = tempVal2;
+                }
+
+                resultados.Add(new Tuple<int, int, int, double, double>(iteracion++, semillaActual, cuadrado, valor1, valor2));
+
+                if (semillaActual == 0)
+                {
+                    break;
+                }
+
+                semillaActual = (int)valor1;
+            }
+
+            return resultados;
+        }
+    }
+}
---
+++ MCuadradoMedio/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MCuadradoMedio.Algoritmos;
+
+
+namespace MCuadradoMedio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Add("Semillas", "N");
+            dataGridView1.Columns.Add("Semillas", "R(n)");
+            dataGridView1.Columns.Add("Cuadrados", "R(n)^2");
+            dataGridView1.Columns.Add("Valor1", "Valor 1");
+            dataGridView1.Columns.Add("Valor2", "Valor 2");
+        }
+
+
+        public void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int semilla) && semilla >= 100 && semilla <= 999)
+            {
+                AlgoritmoGenerador generador = new AlgoritmoGenerador();
+                List<Tuple<int, int, int, double, double>> resultados = generador.MetodoCuadradosMedios(semilla);
+
+                dataGridView1.Rows.Clear();
+                foreach (Tuple<int, int, int, double, double> resultado in resultados)
+                {
+                    dataGridView1.Rows.Add(resultado.Item1.ToString(), resultado.Item2.ToString(), resultado.Item3.ToString(), resultado.Item4.ToString(), resultado.Item5.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese una semilla de 3 dígitos.");
+            }
+        }
+    }
+}
